8c8100461642f6649a4e6286f100b43e02d8b28c
soeun lee <thdmsdl87@gmail.com>
2021-01-10 23:52:24 +0900

    feat(common): BOJ 17225 for common challenge leesoeun98, 140 version

--- src/common/leesoeun98_baekjoon17225.java
+++ src/common/leesoeun98_baekjoon17225.java
@@ -1,100 +1,110 @@
 package common;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class leesoeun98_baekjoon17225 {
+	static class Pair implements Comparable<Pair> {
+		int startTime;
+		String color;
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int n = scan.nextInt();
-		int jgift = 0;
-		int sgift = 0;
-		int scount = 0;
-		int jcount = 0;
+		public Pair(int startTime, String color) {
+			this.startTime = startTime;
+			this.color = color;
+		}
+
+		/*
+		 * 우선순위 큐를 사용자지정 자료형으로 구현 시, compareTo를 오버라이드 해야 하는데 B가 R보다 우선순위가 높도록 작성해야 한다.
+		 * this.color와 p.color를 비교해서 자기자신(B)-상대(R)이면 -1을, 반대면 1을, 같으면 0을 반환, -1일 때 우선순위가
+		 * 높아서 자기자신이 앞에온다.
+		 */
+		@Override
+		public int compareTo(Pair p) {
+			if (this.startTime == p.startTime) {
+				if (this.color.equals("B") && p.color.equals("R")) {
+					return -1;
+				} else if (this.color.equals("R") && p.color.equals("B")) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+			return Integer.compare(this.startTime, p.startTime);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		//
+		int[] first = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		int a = first[0];
+		int b = first[1];
+		int n = first[2];
+
 		int gift = 1;
+
 		int maxs = -1;
 		int maxj = -1;
 
-		ArrayList<Integer> sangmin = new ArrayList<Integer>();
-		ArrayList<Integer> jisoo = new ArrayList<Integer>();
+		PriorityQueue<Pair> pq = new PriorityQueue<>(Pair::compareTo);
 		ArrayList<Integer> sangmingift = new ArrayList<Integer>();
 		ArrayList<Integer> jisoogift = new ArrayList<Integer>();
 
 		for (int i = 0; i < n; i++) {
-			int time = scan.nextInt();
-			String color = scan.next();
-			int m = scan.nextInt();
+			String[] line = br.readLine().split(" ");
+			int time = Integer.parseInt(line[0]);
+			int m = Integer.parseInt(line[2]);
 			int cnt = 0;
-			if (color.equals("B")) {
+			if (line[1].equals("B")) {
 				// 앞의 선물 포장 끝나고 다음 포장 시작 고려
 				if (maxs > time)
 					time = maxs;
 				for (int j = time; cnt < m; j += a) {
-					sangmin.add(j);
+					pq.add(new Pair(j, "B"));
 					cnt += 1;
-					sgift += 1;
 				}
 				maxs = time + m * a;
 			} else {
 				if (maxj > time)
 					time = maxj;
 				for (int j = time; cnt < m; j += b) {
-					jisoo.add(j);
+					pq.add(new Pair(j, "R"));
 					cnt += 1;
-					jgift += 1;
 				}
 				maxj = time + m * b;
 			}
 
 		}
 
-		// 시간 순대로 선물 순서 정하는 부분
-		while (jgift - 1 >= jcount && sgift - 1 >= scount) {
-
-			if (sangmin.get(scount) == jisoo.get(jcount)) {
-				sangmingift.add(gift);
-				gift++;
-				scount++;
-				jisoogift.add(gift);
-				gift++;
-				jcount++;
-			} else if (sangmin.get(scount) > jisoo.get(jcount)) {
-				jisoogift.add(gift);
-				gift++;
-				jcount++;
+		while (!pq.isEmpty()) {
+			Pair pair = pq.poll();
+			if (pair.color.equals("B")) {
+				sangmingift.add(gift++);
 			} else {
-				// 나중이므로 상민이 선물이 먼저
-				sangmingift.add(gift);
-				gift++;
-				scount++;
+				jisoogift.add(gift++);
 			}
-		}
-		// 마지막 값 출력
-		while (jcount < jgift) {
-			jisoogift.add(gift);
-			jcount++;
-			gift++;
-		}
-		while (scount < sgift) {
-			sangmingift.add(gift);
-			scount++;
-			gift++;
 		}
 
 		// 정답 출력
-		System.out.println(scount);
+		bw.write(sangmingift.size() + "\n");
 		for (int i : sangmingift) {
-			System.out.print(i + " ");
+			bw.write(i + " ");
 		}
-		System.out.println();
-		System.out.println(jcount);
+		bw.write("\n");
+		bw.write(jisoogift.size() + "\n");
 		for (int i : jisoogift) {
-			System.out.print(i + " ");
+			bw.write(i + " ");
 		}
-
+		bw.flush();
+		bw.close();
 	}
 
 }
