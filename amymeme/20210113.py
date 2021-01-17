88d63105ed86c40c6f8dc7b3b86cacb3697af298
amy choi <cgc8016@ewhain.net>
2021-01-18 02:11:12 +0900

    feat(20210113): BOJ 19640 (solve timeout)

--- amymeme/20210113.py
+++ amymeme/20210113.py
@@ -1,27 +1,32 @@
 # 백준 #19640
 # https://www.acmicpc.net/problem/19640
 
+import heapq
 import sys
-from itertools import repeat, islice
+from itertools import repeat
 
 
 N, M, K = map(int, sys.stdin.readline().split())
 lines = [[] for i in repeat(None, M)]
-result = 0
+count = 0
 
 for original_turn in range(N):
     D, H = map(int, sys.stdin.readline().split())
-    lines[(original_turn + M) % M].append([D, H, original_turn])
+    lines[original_turn % M].append((D, H, original_turn%M, original_turn))
 
-while lines:
-    next_turn = [0, 0]
-    next_index = 0
+head = []
 
-    for i in range(M):
-        if lines[i] and lines[i][0][:2] > next_turn[:2]:
-            next_turn = lines[i][0]
-            next_index = i
-    if lines[next_index].pop(0)[2] == K:
-        print(result)
+for line in lines:
+    if line:
+        heapq.heappush(head, ((-line[0][0], -line[0][1], line[0][2]), line[0][3]))
+
+while head:
+    now_turn = heapq.heappop(head)
+    lines[now_turn[0][2]].pop(0)
+    if now_turn[1] == K:
         break
-    result += 1
+    if lines[now_turn[0][2]]:
+        next_turn = lines[now_turn[0][2]][0]
+        heapq.heappush(head, ((-next_turn[0], -next_turn[1], next_turn[2]), next_turn[3]))
+    count += 1
+print(count)
