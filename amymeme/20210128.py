873d593ce32143df8d9e5224746d20c3d9d0e141
amy choi <cgc8016@ewhain.net>
2021-01-26 00:59:58 +0900

    feat(personal): use dictionary

--- amymeme/20210128.py
+++ amymeme/20210128.py
@@ -1,53 +1,57 @@
-import heapq
+# https://www.acmicpc.net/problem/16235
+# 백준 16235 - 나무 재태크
+
 import sys
 
-
-def spring_and_summer():
-    next_tree_infos = []
-    dead_trees = []
-    while tree_infos:
-        x, y, z = heapq.heappop(tree_infos)
-        if land[x][y] >= z:
-            land[x][y] -= z
-            heapq.heappush(next_tree_infos, (x, y, z + 1))
-        else:
-            dead_trees.append((x, y, z))
-
-    for x, y, z in dead_trees:
-        land[x][y] += (z // 2)
-    return next_tree_infos
-
-
-def autumn():
-    tmp = []
-    while tree_infos:
-        x, y, z = heapq.heappop(tree_infos)
-        heapq.heappush(tmp, (x, y, z))
-        if z % 5 != 0:
-            continue
-        for i in range(8):
-            near_x, near_y = x + dx[i], y + dy[i]
-            if -1 < near_x < N and -1 < near_y < N:
-                heapq.heappush(tmp, (near_x, near_y, 1))
-    return tmp
-
-
-def winter():
-    return [[land[i][j] + fertilizers[i][j] for j in range(N)] for i in range(N)]
-
-
-dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+diff = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, -1), (-1, 1)]
 N, M, K = map(int, sys.stdin.readline().split())
 land = [[5] * N for _ in range(N)]
 fertilizers = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-tree_infos = []
+tree_infos = [[{} for _ in range(N)] for _ in range(N)]
+
 for _ in range(M):
     x, y, z = map(int, sys.stdin.readline().split())
-    heapq.heappush(tree_infos, (x - 1, y - 1, z))
+    tree_infos[x - 1][y - 1][z] = 1  # 개수
 
 for _ in range(K):
-    tree_infos = spring_and_summer()
-    tree_infos = autumn()
-    land = winter()
-print(len(tree_infos))
+    for i in range(N):
+        for j in range(N):
+            if tree_infos[i][j]:
+                temp = {}
+                dead = 0
+                for age in sorted(tree_infos[i][j].keys()):
+                    if age * tree_infos[i][j][age] <= land[i][j]:
+                        land[i][j] -= age * tree_infos[i][j][age]
+                        temp[age + 1] = tree_infos[i][j][age]
+                    else:
+                        survive = land[i][j] // age
+                        if not survive:
+                            dead += (age // 2) * tree_infos[i][j][age]
+                            continue
+                        land[i][j] -= age * survive
+                        temp[age + 1] = survive
+                        dead += (age // 2) * (tree_infos[i][j][age] - survive)
+                tree_infos[i][j] = temp
+                land[i][j] += dead
+            land[i][j] += fertilizers[i][j]
+    for i in range(N):
+        for j in range(N):
+            if tree_infos[i][j]:
+                count = 0
+                for age in tree_infos[i][j].keys():
+                    if age % 5 == 0:
+                        count += tree_infos[i][j][age]
+                if count:
+                    for dx, dy in diff:
+                        nx, ny = i + dx, j + dy
+                        if -1 < nx < N and -1 < ny < N:
+                            if 1 not in tree_infos[nx][ny].keys():
+                                tree_infos[nx][ny][1] = count
+                            else:
+                                tree_infos[nx][ny][1] += count
+
+count = 0
+for i in range(N):
+    for j in range(N):
+        count += sum(tree_infos[i][j].values())
+print(count)
