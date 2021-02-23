1dfabcd4a1a9ade00590cfe823a53a6fcd466210
amy choi <cgc8016@ewhain.net>
2021-02-23 20:17:22 +0900

    feat(1339): 단어 수학 success.ver

--- amymeme/20210224/1339.py
+++ amymeme/20210224/1339.py
@@ -1,36 +1,25 @@
 # https://www.acmicpc.net/problem/1339
 # 백준 1339 - 단어 수학
-
+# 참고 : https://suri78.tistory.com/183
 
 import sys
-from itertools import chain
+from collections import defaultdict
 
 N = int(sys.stdin.readline())
 words = [sys.stdin.readline().strip() for _ in range(N)]
-_words = words.copy()
-word_number_dic = dict()
-digits = []
+dic = defaultdict(int)
 max_number = 9
 result = 0
 
-while list(chain(*_words)):
-    digit = []
-    for idx, word in enumerate(_words):
-        if not word:
-            continue
-        digit.append(word[-1])
-        _words[idx] = word[:-1]
-    digits.append(digit)
-
-for digit in digits[::-1]:
-    for char in digit:
-        if char not in word_number_dic.keys():
-            word_number_dic[char] = max_number
-            max_number -= 1
-print(word_number_dic)
 for word in words:
-    number = 0
     for idx, char in enumerate(word):
-        number += word_number_dic[char] * pow(10, (len(word) - idx - 1))
-    result += number
+        dic[char] += pow(10, (len(word) - idx - 1))
+
+digits = list(dic.values())
+digits.sort(reverse=True)
+
+for digit in digits:
+    digit *= max_number
+    max_number -= 1
+    result += digit
 print(result)
