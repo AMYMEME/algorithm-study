9aedf84b3ca4886d834e16849d1cd55a2b033561
soeun lee <thdmsdl87@gmail.com>
2021-03-22 18:30:46 +0900

    feat(21.03.22): Update BOJ 1916

--- common/2021.03.16/leesoeun98_baekjoon1916.java
+++ common/2021.03.16/leesoeun98_baekjoon1916.java
@@ -4,51 +4,66 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class baekjoon1916 {
+
+    static int v, e;
+    static int[][] adj;
+    static long distance[];
+    static boolean check[];
+    static int start, end;
+
     static public void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int v = sc.nextInt(); // 노드 수 == 도시 수
-        int e = sc.nextInt(); // 간선 수 == 버스 수
-        int[][] adj = new int[v+1][v+1];
+        v = sc.nextInt(); // 노드 수 == 도시 수
+        e = sc.nextInt(); // 간선 수 == 버스 수
+        adj = new int[v + 1][v + 1];
+        for (int[] arr : adj) Arrays.fill(arr, -1);
         for (int i = 0; i < e; i++) {
-            adj[sc.nextInt()][sc.nextInt()] = sc.nextInt();
-        }
-        int start = sc.nextInt();
-        int end = sc.nextInt();
-
-        int distance[] = new int[v+1];
-        boolean check[] = new boolean[v+1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-
-        //시작 노드 값 초기화
-        distance[start] = 0;
-        check[start] = true;
-
-        //distance 갱신
-        for(int i=1;i<v+1;i++){
-            if(!check[i] && adj[start][i]!=0){
-                distance[i]=adj[start][i];
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            int price = sc.nextInt();
+            //여기 중요, 동일한 경로에 다양한 price 들어옴
+            if (adj[from][to] == -1) {
+                adj[from][to] = price;
+            } else if (adj[from][to] > price) {
+                adj[from][to] = price;
             }
         }
+        start = sc.nextInt();
+        end = sc.nextInt();
 
-        for (int a = 0; a < v - 1; a++) {
-            int min = Integer.MAX_VALUE;
-            int min_index = -1;
-            //최소값 찾기
-            for (int i = 1; i < v + 1; i++) {
-                if (!check[i] && distance[i] < min) {
-                        min = distance[i];
-                        min_index = i;
-                }
-            }
-            //check를 true로
-            check[min_index]=true;
-            //distance 배열 갱신
-            for (int i = 1; i < v + 1; i++) {
-                if (!check[i] && adj[min_index][i]!=Integer.MAX_VALUE && adj[min_index][i]!=0 && distance[i] > distance[min_index]+adj[min_index][i]) {
-                    distance[i] = distance[min_index]+adj[min_index][i];
-                }
-            }
+        distance = new long[v + 1];
+        check = new boolean[v + 1];
+        for(int i=1;i<=v;i++){
+            //-1 해야함
+            distance[i]=Integer.MAX_VALUE-1;
+            check[i]=false;
         }
+        dijkstra();
         System.out.println(distance[end]);
+    }
+
+    public static void dijkstra() {
+        distance[start] = 0;
+
+        for (int i = 0; i < v - 1; i++) {
+            long min = Integer.MAX_VALUE;
+            int from = -1;
+
+            //min, minIndex 갱신
+            for (int j = 1; j <= v; j++) {
+                if (!check[j] && min > distance[j]) {
+                    min = distance[j];
+                    from = j;
+                }
+            }
+            check[from] = true;
+
+            for (int to = 1; to <= v; to++) {
+                //-1 해야함
+                if(distance[from]!=Integer.MAX_VALUE-1 && adj[from][to]!=-1 && distance[to]>distance[from]+adj[from][to]){
+                    distance[to]=distance[from]+adj[from][to];
+                }
+            }
+        }
     }
 }
