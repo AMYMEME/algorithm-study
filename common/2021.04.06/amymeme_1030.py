ce732f7e15e0585063679aa58530e3b7f2a633d9
amy choi <cgc8016@ewhain.net>
2021-04-03 03:30:42 +0900

    feat(1030): 프랙탈 평면 (success ver.)

--- common/2021.04.06/amymeme_1030.py
+++ common/2021.04.06/amymeme_1030.py
@@ -4,33 +4,33 @@
 import sys
 
 
-def fractal():
-    global board
-    new_board = []
-    board_len = len(board)
-    for i in range(N):
-        if i < middle_start or i >= middle_start + K:
-            for row in board:
-                new_board.append(row * N)
+def is_middle(board_len, i, j):
+    n_times = board_len // N
+    if n_times * middle_start <= i < n_times * middle_end:
+        return n_times * middle_start <= j < n_times * middle_end
+    return False
+
+
+def check(board_len, i, j):
+    if board_len == 1:
+        print(0, end='')
+        return
+    if is_middle(board_len, i, j):
+        print(1, end='')
+    else:
+        if board_len == N:
+            print(0, end='')
         else:
-            for row in board:
-                new_row = row * middle_start + [1] * board_len * K + row * middle_start
-                new_board.append(new_row)
-    board = new_board
+            next_len = board_len // N
+            check(next_len, i % next_len, j % next_len)
 
 
 s, N, K, R1, R2, C1, C2 = map(int, sys.stdin.readline().split())
 middle_start = (N - K) // 2
-board = [[0]]
+middle_end = middle_start + K
+final_len = pow(N, s)
 
-clock = 0
-while clock < s:
-    clock += 1
-    fractal()
-
-only_rows = board[R1:R2 + 1]
-for row in only_rows:
-    for c_idx, value in enumerate(row):
-        if C1 <= c_idx <= C2:
-            print(value, end='')
+for i in range(R1, R2 + 1):
+    for j in range(C1, C2 + 1):
+        check(final_len, i, j)
     print()
