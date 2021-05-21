2df64e101875696e98e397881ceb9ee698735228
amy choi <cgc8016@ewhain.net>
2021-05-21 16:56:14 +0900

    feat(섬 연결하기): Kruskal algorithm ver.

--- amymeme/20210525/섬 연결하기.py
+++ amymeme/20210525/섬 연결하기.py
@@ -1,29 +1,52 @@
-import heapq
-from collections import defaultdict
+# https://programmers.co.kr/learn/courses/30/lessons/42861
+
+from collections import deque
+
+parent = []
 
 
-def prim(n, graph, start=0):
-    distances = dict()
-    distances[start] = 0
-    visited = {start}
-    while len(visited) < n:
-        pq = []
-        for v in visited:
-            for adj_v, weight in graph[v]:
-                if adj_v in visited:
-                    continue
-                heapq.heappush(pq, (weight, adj_v))
-        distance, next_visit = heapq.heappop(pq)
-        distances[next_visit] = distance
-        visited.add(next_visit)
-    return distances
+def find_parent(v1):
+    global parent
+    if parent[v1] != v1:
+        return find_parent(parent[v1])
+    return parent[v1]
+
+
+def union(v1, v2):
+    global parent
+    v1_parent = find_parent(v1)
+    v2_parent = find_parent(v2)
+    if v1_parent < v2_parent:
+        parent[v2_parent] = v1_parent
+    else:
+        parent[v1_parent] = v2_parent
+
+
+def kruskal(n, distances):
+    cnt = 0
+    answer = 0
+    while cnt < n - 1:
+        for i in range(len(distances)):
+            distance, v1, v2 = distances[i]
+            if find_parent(v1) == find_parent(v2):
+                continue
+            # not cycle
+            union(v1, v2)
+            distances.pop(i)
+            answer += distance
+            cnt += 1
+            break
+    return answer
 
 
 def solution(n, costs):
-    graph = defaultdict(list)
+    global parent
+    for i in range(n):
+        # initial parent
+        parent.append(i)
+    distances = deque([])
     for cost in costs:
         v1, v2, weight = cost
-        graph[v1].append((v2, weight))
-        graph[v2].append((v1, weight))
-    distances = prim(n, graph)
-    return sum(distances.values())
+        distances.append((weight, v1, v2))
+
+    return kruskal(n, sorted(distances))
