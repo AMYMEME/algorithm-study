415ce9ff6656479573d39e93e9cf4faa3941f486
maplejh <cjh0313c@gmail.com>
2021-05-22 21:10:46 +0900

    feat(20210525): BOJ 17281

--- common/2021.05.25/maplejh_17281.py
+++ common/2021.05.25/maplejh_17281.py
@@ -7,22 +7,40 @@ from itertools import permutations
 
 def game(o):
     s = 0
-    o.insert(3, 0)
+    o.insert(3, 0)  # 타순
     n = 0
-    for res in inns.values():
-        bs = 0
+    for res in inns.values():  # 각 이닝
+        bs1 = 0
+        bs2 = 0
+        bs3 = 0
         out = 0
         while True:
             n %= 9
             player = o[n]
             if out == 3:
-                break
-            if res[player] == 0:
+                break  # 이닝 종료
+            if res[player] == 0:  # 아웃
                 out += 1
-            else:
-                bs = (bs << 1 | 1) << (res[player] - 1)
-                s += bin(bs >> 3).count('1')
-                bs = bs & 7
+            elif res[player]==1:
+                s+= bs3
+                bs3=bs2
+                bs2=bs1
+                bs1=1
+            elif res[player]==2:
+                s+=bs3+bs2
+                bs3=bs1
+                bs2=1
+                bs1=0
+            elif res[player]==3:
+                s += (bs1 + bs2 + bs3)
+                bs3=1
+                bs2=0
+                bs1=0
+            elif res[player]==4:
+                s+=(bs1+bs2+bs3+1)
+                bs1=0
+                bs2=0
+                bs3=0
             n += 1
     return s
 
