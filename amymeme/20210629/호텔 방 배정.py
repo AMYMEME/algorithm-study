2aad851d6f6e2beef2aeccf84165d5bd700b8e6b
amy choi <cgc8016@ewhain.net>
2021-06-28 16:44:56 +0900

    feat(호텔 방 배정): success ver.

--- amymeme/20210629/호텔 방 배정.py
+++ amymeme/20210629/호텔 방 배정.py
@@ -1,13 +1,25 @@
 # https://programmers.co.kr/learn/courses/30/lessons/64063
+# https://m.blog.naver.com/js568/221957852279 참고
 # 호텔 방 배정
 
-def solution(k, room_number):
-    answer = [0] * len(room_number)
-    empty_rooms = [True] * (k + 1)
-    for idx, want in enumerate(room_number):
-        while not empty_rooms[want]:
-            want += 1
-        empty_rooms[want] = False
-        answer[idx] = want
+import sys
 
-    return answer
+sys.setrecursionlimit(10000)  # 재귀 허용깊이 임의로 지정
+
+rooms = dict()
+
+
+def solution(k, room_number):
+    for num in room_number:
+        find_empty(num)
+    return list(rooms.keys())
+
+
+def find_empty(chk):  # 재귀함수
+    global rooms
+    if chk not in rooms.keys():  # 빈 방이면
+        rooms[chk] = chk + 1  # rooms에 새 노드 추가
+        return chk  # 요청한 방
+    empty = find_empty(rooms[chk])  # 재귀함수 호출
+    rooms[chk] = empty + 1  # (배정된 방+1)을 부모노드로 변경
+    return empty  # 새로 찾은 빈 방
