7190ad76e7f4546b11eb964a6877dacef763b258
amy choi <cgc8016@ewhain.net>
2021-07-01 00:24:07 +0900

    feat(1976): 여행 가자 Floyd-Warshall ver.

--- common/2021.07.06/amymeme_1976.py
+++ common/2021.07.06/amymeme_1976.py
@@ -1,45 +1,30 @@
 # https://www.acmicpc.net/problem/1976
 # 여행 가자
-from collections import deque, defaultdict
+from collections import deque
 import sys
-
-
-def bfs(v):
-    q = deque([v])
-    visited = {v}
-
-    while q:
-        v = q.popleft()
-        for next_v in graph[v]:
-            if next_v in visited:
-                continue
-            q.append(next_v)
-            visited.add(next_v)
-    return visited
-
 
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 
-graph = defaultdict(set)
-for start in range(N):
-    row = map(int, sys.stdin.readline().split())
-    for end, value in enumerate(row):
-        if value:
-            graph[start].add(end)
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+for x in range(N):
+    board[x][x] = 1
 
-want_cities = deque(map(lambda x: int(x) - 1, sys.stdin.readline().split()))
+for x in range(N):
+    for i in range(N):
+        for j in range(N):
+            if board[i][x] and board[x][j] and not board[i][j]:
+                board[i][j] = 1
+                board[j][i] = 1
 
-connected = defaultdict(set)
-
-bfs_checked = set()
+print(board)
+want_cities = deque(map(lambda k: int(k) - 1, sys.stdin.readline().split()))
+flag = 'YES'
 while want_cities:
-    cur_city = want_cities.popleft()
+    want_city = want_cities.popleft()
     if not want_cities:
         break
-    if cur_city not in bfs_checked:
-        connected[cur_city] = bfs(cur_city)
-    if not want_cities[0] in connected[cur_city]:
-        print('NO')
-        exit(0)
-print('YES')
+    if not board[want_city][want_cities[0]]:
+        flag = 'NO'
+        break
+print(flag)
