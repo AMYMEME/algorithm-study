49c7bf3f962b7149918261ec46fb2a04da142fd8
amy choi <cgc8016@ewhain.net>
2021-07-02 23:18:50 +0900

    feat(징검다리 건너기): success(bisect) ver.

--- amymeme/20210706/징검다리 건너기.py
+++ amymeme/20210706/징검다리 건너기.py
@@ -1,15 +1,24 @@
 # https://programmers.co.kr/learn/courses/30/lessons/64062
 # 징검다리 건너기
+def check(stones, less_than):
+    cnt = 0
+    max_cnt = 0
+    for idx, stone in enumerate(stones):
+        if stone <= less_than:
+            cnt += 1
+            max_cnt = max(max_cnt, cnt)
+        else:
+            cnt = 0
+    return max_cnt
+
+
 def solution(stones, k):
-    answer = 0
-    while True:
-        cnt = 0
-        for idx, stone in enumerate(stones):
-            if stone:
-                stones[idx] = stone - 1
-                cnt = 0
-            else:
-                cnt += 1
-                if cnt >= k:
-                    return answer
-        answer += 1
+    left = 0
+    right = max(stones)
+    while left < right:
+        mid = (left + right) // 2
+        if check(stones, mid) < k:
+            left = mid + 1
+        else:
+            right = mid
+    return left
