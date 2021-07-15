46496088898be83508359770905eabfbefaef27d
jangayeon <ayeon3052@email.com>
2021-07-15 23:09:47 +0900

    2021.07.15

---
+++ common/2021.07.20/jangayeon_1197.py
@@ -0,0 +1,4 @@
+#문제: https://www.acmicpc.net/problem/1197
+
+최소 신장 트리
+www.youtube.com/watch?v=LQ3JHknGy8c
---
+++ common/2021.07.20/jangayeon_1504.py
@@ -0,0 +1,3 @@
+#문제:https://www.acmicpc.net/problem/1504
+
+두 정점 1->2 또는 2->1 둘 다 고려해서 더 작은걸로 출력
