a88f5613a3c907854d65d40f088382406f09807d
jangayeon <ayeon3052@email.com>
2021-07-17 23:07:04 +0900

    20210717

--- common/2021.07.20/jangayeon_1197.py
+++ common/2021.07.20/jangayeon_1197.py
@@ -1,4 +1,46 @@
 #문제: https://www.acmicpc.net/problem/1197
 
-최소 신장 트리
-www.youtube.com/watch?v=LQ3JHknGy8c
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def make_union(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+V, E = map(int, input().split())
+graph = []
+
+parent = [0] * (V + 1)
+for i in range(V + 1):
+    parent[i] = i
+
+for _ in range(E):
+    A, B, C = map(int, input().split())
+    graph.append([C, A, B])
+
+
+graph.sort()
+count = 0
+answer = 0
+
+while graph:
+    if count == (V-1):
+        break
+
+    C,A,B = graph.pop(0)
+
+    if find_parent(parent, A) == find_parent(parent, B):
+        continue
+    else:
+        make_union(parent, A, B)
+        count += 1
+        answer += C
+print(answer)
