b72777e5bb7de9799d0b56663cf9907c69a1344b
jangayeon <ayeon3052@email.com>
2021-07-22 23:24:42 +0900

    20210722

--- common/2021.07.20/jangayeon_1644.py
+++ common/2021.07.20/jangayeon_1644.py
@@ -1,4 +1,39 @@
 #문제:https://www.acmicpc.net/problem/1644
 
+"""
 소수를 위한 에라토스체
 https://mygumi.tistory.com/66
+"""
+
+import sys
+input = sys.stdin.readline
+ 
+N = int(input())
+isPrime = [False, False] + [True] * (N - 1)
+Seqprime = []
+ 
+for i in range(2, N + 1):
+    if isPrime[i]:
+        Seqprime.append(i)
+        for j in range(2 * i, N + 1, i):
+            isPrime[j] = False
+ 
+start = 0
+end = 0
+sum = 0
+ans = 0
+ 
+while(True):
+    if sum >= N:
+        sum -= Seqprime[start]
+        start += 1
+    elif end == len(Seqprime):
+        break
+    else:
+        sum += Seqprime[end]
+        end += 1
+ 
+    if sum == N:
+        ans += 1
+ 
+print(ans)
---
+++ common/2021.07.21/jangayeon_2252.py
@@ -0,0 +1,3 @@
+#문제: https://www.acmicpc.net/problem/2252
+
+위상 정렬 그래프 순회
