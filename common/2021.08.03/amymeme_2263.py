5b4e791d1fcbc3c44ac543aef6a7453dfd5fb9ba
amy choi <cgc8016@ewhain.net>
2021-08-01 01:59:53 +0900

    feat(2263): 트리의 순회 (success ver.)

--- common/2021.08.03/amymeme_2263.py
+++ common/2021.08.03/amymeme_2263.py
@@ -2,30 +2,31 @@
 # 트리의 순회
 
 import sys
+from collections import defaultdict
 
 
-def start(inorder, postorder):
+def start(in_left, in_right, post_left, post_right):
     global pre_order
-    root_value = postorder.pop()
+    root_value = post_order[post_right]
     pre_order.append(root_value)
-    left_count = 0
+    index = in_order_dict[root_value]
 
-    for idx, value in enumerate(inorder):
-        if value == root_value:
-            left_count = idx
-            break
+    if in_left < index:
+        left_cnt = index - 1 - in_left
+        start(in_left, in_left + left_cnt, post_left, post_left + left_cnt)
 
-    if left_count > 0:  # left exist
-        start(inorder[:left_count], postorder[:left_count])
-
-    if left_count < len(inorder) - 1:
-        start(inorder[left_count + 1:], postorder[left_count:])
+    if index < in_right:
+        right_cnt = in_right - index - 1
+        start(index + 1, index + 1 + right_cnt, post_right - 1 - right_cnt, post_right - 1)
 
 
 sys.setrecursionlimit(10 ** 5)
 n = int(sys.stdin.readline())
+in_order_dict = defaultdict(int)
 in_order = list(map(int, sys.stdin.readline().split()))
+for idx, value in enumerate(in_order):
+    in_order_dict[value] = idx
 post_order = list(map(int, sys.stdin.readline().split()))
 pre_order = []
-start(in_order, post_order)
+start(0, n - 1, 0, n - 1)
 print(*pre_order, sep=' ')
