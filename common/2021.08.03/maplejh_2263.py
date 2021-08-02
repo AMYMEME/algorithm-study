74c71faa5f430b560df6d673c9438b7b7f8a1cf0
maplejh <cjh0313c@gmail.com>
2021-08-02 21:38:23 +0900

    feat(20210803): BOJ 2263

--- common/2021.08.03/maplejh_2263.py
+++ common/2021.08.03/maplejh_2263.py
@@ -1,22 +1,29 @@
 # https://www.acmicpc.net/problem/2263
 import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(10 ** 9)
 
 
-def preorder(postorder, inorder):
-    root = postorder[-1]
-    loc = inorder.index(root)  # root 위치 - 트리 나누기
-    left = inorder[:loc]
-    right = inorder[loc + 1:]
+def preorder(in_start, in_end, post_start, post_end):
+    if in_start > in_end or post_start > post_end:
+        return
+    root = postorder[post_end]
+    loc = idx[root]  # root 위치 - 트리 나누기
+    left_len = loc - in_start  # root를 기준으로 inorder에서 왼쪽
+    right_len = in_end - loc  # root를 기준으로 inorder에서 오른쪽
     answer.append(root)
-    if postorder[:loc]:
-        preorder(postorder[:loc], left)
-    if postorder[loc:-1]:
-        preorder(postorder[loc:-1], right)
+    preorder(in_start, loc - 1, post_start, post_start + left_len - 1)
+    preorder(loc + 1, in_end, post_end - right_len, post_end - 1)
 
 
 n = int(sys.stdin.readline())
-i = list(map(int, sys.stdin.readline().split()))
-p = list(map(int, sys.stdin.readline().split()))
+inorder = list(map(int, sys.stdin.readline().split()))
+postorder = list(map(int, sys.stdin.readline().split()))
+idx = defaultdict(int)
+for k in range(n):
+    idx[inorder[k]] = k
+
 answer = []
-preorder(p, i)
+preorder(0, n - 1, 0, n - 1)
 print(*answer)
