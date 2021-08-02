cb6c1d3a33e143c2c601c9e9e63c39fa01356ac0
amy choi <cgc8016@ewhain.net>
2021-08-02 23:06:19 +0900

    feat(1520): 내리막 길 (bfs, pq, dp)

--- common/2021.08.03/amymeme_1520.py
+++ common/2021.08.03/amymeme_1520.py
@@ -3,27 +3,37 @@
 
 
 import sys
-from collections import deque
+import heapq
 
 diff = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 M, N = map(int, sys.stdin.readline().split())
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+dp = [[0] * N for _ in range(M)]
 
-goal_value = board[-1][-1]
-q = deque([(board[0][0], 0, 0)])  # value, i , j
-count = 0
+pq = []
+dp[0][0] = 1
+heapq.heappush(pq, (-board[0][0], 0, 0))  # min heap
 
-while q:
-    value, i, j = q.popleft()
+while pq:
+    value, i, j = heapq.heappop(pq)
     value *= -1
+
     for di, dj in diff:
         ni = i + di
         nj = j + dj
         if -1 < ni < M and -1 < nj < N and board[ni][nj] < value:
-            if ni == M - 1 and nj == N - 1:
-                count += 1
+            if dp[ni][nj]:
+                dp[ni][nj] += dp[i][j]
                 continue
-            if board[ni][nj] <= goal_value:
-                continue
-            q.append((board[ni][nj], ni, nj))
-print(count)
+            dp[ni][nj] += dp[i][j]
+            heapq.heappush(pq, (-board[ni][nj], ni, nj))
+
+
+print(dp[-1][-1])
+'''
+4 5
+50 45 37 32 30
+35 50 40 20 25
+30 30 25 17 28
+27 24 22 15 10
+'''
