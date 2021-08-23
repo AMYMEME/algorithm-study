59795cb53b0b6496bfa0b9f8a5f980b703cac9ee
amy choi <cgc8016@ewhain.net>
2021-08-23 18:33:51 +0900

    feat(6549): 히스토그램에서 가장 큰 직사각형

--- common/2021.08.24/amymeme_6549.py
+++ common/2021.08.24/amymeme_6549.py
@@ -1,35 +1,28 @@
 # https://www.acmicpc.net/problem/6549
 # 히스토그램에서 가장 큰 직사각형
 
+# 참고 : https://foramonth.tistory.com/13
+
+
 import sys
-from collections import defaultdict
 
 
 def solution(l):
     N = l[0]
-    connect = defaultdict(int)
-    dp = [l[1]]
-    connect[l[1]] += 1
+    stack = []  # (connect_start_idx, value)
+    max_value = 0
 
-    for idx, h in enumerate(l[2:], start=2):
-        before = l[idx - 1]
-        connect[h] += 1
+    for cur_idx, cur_h in enumerate(l[1:]):
+        min_idx = cur_idx
+        while stack and stack[-1][1] >= cur_h:
+            min_idx, before_h = stack.pop()
+            max_value = max(max_value, before_h * (cur_idx - min_idx))
+        stack.append((min_idx, cur_h))
 
-        if before > h:  # 작아진 경우
-            if connect[h] == 1:
-                connect[h] += 1
-            for k, _ in filter(lambda x: x[0] > h, connect.items()):
-                connect[k] = 0
-            dp.append(max(dp[-1], connect[h] * h))
-
-        elif before <= h:  # 커진 경우
-            before_max = 0
-            for k, _ in filter(lambda x: x[1], connect.items()):
-                if k != h:
-                    connect[k] += 1
-                before_max = max(before_max, connect[k] * k)
-            dp.append(max(dp[-1], before_max))
-    return dp[-1]
+    # square for start_idx ~
+    for start_idx, value in stack:
+        max_value = max(max_value, (N - start_idx) * value)
+    return max_value
 
 
 while True:
@@ -37,3 +30,39 @@ while True:
     if row == [0]:
         break
     print(solution(row))
+
+'''
+7 2 1 4 5 1 3 3
+8 2 1 4 5 4 1 3 3
+4 1000 1000 1000 1000
+5 1 2 3 4 5
+3 1000000000 1000000000 1000000000
+7 0 5 7 5 5 3 1
+4 1 4 3 3
+5 1 3 2 4 6
+4 2 3 2 1
+3 3 4 3
+4 1 3 3 1
+4 0 1 2 3
+1 5
+6 7 0 3 1 0 3
+0
+
+'''
+
+'''
+8
+12
+4000
+9
+3000000000
+20
+9
+8
+6
+9
+6
+4
+5
+7
+'''
